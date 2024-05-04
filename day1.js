8a8268ef0f74db3dc59b4845fea712ee75e21b29
vivek pandey <udyalakvivek@gmail.com>
2024-05-04 12:45:43 +0530

    only html

---
+++ day1.js
@@ -0,0 +1,12 @@
+//   First Question 
+
+let arr1=[1,2,3,4];
+let arr2=[5,6,7,8,9]
+// Using concat method
+
+let mergeArr=arr1.concat(arr2)
+console.log("Merge Array using concat :",mergeArr);
+
+// using Spread operator 
+
+let mergeArr2=[...arr1,...arr2];
